b856fd84ed5c5c68d5e3f3f0d8268ddd9ff928e0
Kavinash Devakumar <nash.kavin.kd@gmail.com>
2025-02-28 21:44:48 +0800

    About artwork
    Additions to the artwork!

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,12 @@ import { motion } from "framer-motion";
 
 //vectors
 import flower from "../assets/images/flowerempty.png";
+import arrow from "../assets/images/arrow.png";
+import gmoth from "../assets/images/green-moth.png";
+import gbutterfly from "../assets/images/light-green-butterly.png";
+import sparklef from "../assets/images/sparkle-for.png";
+import sparklestar from "../assets/images/sparkle-star.png";
+import starsparkle from "../assets/images/star-sparkle.png";
 
 const About = () => {
   const { ref, inView } = useInView({
@@ -16,6 +22,7 @@ const About = () => {
 
   const educationData = [
     {
+      header: "Diploma",
       title: "Diploma in Computer Science",
       institution: "INTI College Penang, MY",
       year: "2020 - 2022",
@@ -23,6 +30,7 @@ const About = () => {
       description2: "Dean's List of April 2022 (GPA 3.98)",
     },
     {
+      header: "Degree",
       title: "Bachelors in Computing with Honors",
       institution: "Coventry University, UK",
       year: "2022 - 2024",
@@ -32,7 +40,10 @@ const About = () => {
   ];
 
   return (
+    
     <section id="about" className="about-section" ref={ref}>
+            {/* <img src={arrow} className="arrow-g" alt="arrow-green"/> */}
+
       <div className="left">
         <div className="intro-text">
           <h1> Hello, </h1>
@@ -63,18 +74,21 @@ const About = () => {
             <SearchOutlined className="icon" />
             linkedin.com/in/kavinash-devakumar/
           </button>
+          <img className="flowerabout" src={gbutterfly} alt="flower"/>
+          {/* <img className="flowerabout2" src={flower} alt="flower"/> */}
         </div>
       </div>
       {/* Right Side */}
       <div className="right">
         <h2 className="education-title"> Education </h2>
         <div className="timeline-container" ref={ref}>
+          
           {inView && (
             <>
               <motion.div
                 initial={{ height: 0 }}
                 animate={{ height: "80vh" }}
-                transition={{ duration: 1.5, ease: "easeInOut" }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
                 className="timeline-line"
               >
                 {[0, 1].map((pos, index) => (
@@ -94,7 +108,7 @@ const About = () => {
                   key={index}
                   initial={{ opacity: 0, x: index % 2 === 0 ? -150 : 150 }}
                   animate={{ opacity: 1, x: 0 }}
-                  transition={{ delay: index + 1, duration: 1 }}
+                  transition={{ delay: index + 0.5, duration: 0.5 }}
                   className={`timeline-box ${
                     index % 2 === 0 ? "left" : "right"
                   }`}
@@ -105,6 +119,7 @@ const About = () => {
                   }}
                 >
                   <div className="timeline-circle">
+                      <h1> {data.header}</h1>
                     <h3>{data.title}</h3>
                     <h2>{data.institution}</h2>
                     <h4>{data.year}</h4>
@@ -116,8 +131,12 @@ const About = () => {
             </>
           )}
         </div>
+        <img className="sparklefabout" src={sparklef} alt="sparkle"/>
+        <img className="sparklefabout2" src={starsparkle} alt="sparkle"/>
+        <img className="sparklefabout3" src={sparklestar} alt="sparkle"/>
       </div>
     </section>
+    
   );
 };
 
